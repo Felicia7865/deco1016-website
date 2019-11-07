09ee39826fed5c32ce97a88446d04b46a292c3e7
Felicia7865 <49709911+Felicia7865@users.noreply.github.com>
2019-11-07 23:27:27 +1100

    forms and script

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
 function MembershipNo(){
+    //var x = document.getElementById("radioNO");
+    //var y = document.getElementById("membertext");
 
-    if (radioNO.checked ){
+   if (radioNO.checked ){
         membertext.style.display = "block";
 
     } else{
         membertext.style.display = "none";
     }
     
+
 }
 
+function checkSubmitType(){
 
+    if (document.getElementById('radioME').checked){
+        window.location = "";
+    }
 
+    else if (document.getElementById('radioSOME').checked){
+        window.location = "";
+    }
+}
